6ca8c0df251dda80fc83c3477bab2aac258832f6
Nhan Phung Tri <ptrinhan@tma.com.vn>
2021-03-02 09:59:06 +0700

    c4

---
+++ src/Common_Methods.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+class car implements Comparable<car>{
+    public String name;
+    public double speed;
+    public double weight;
+
+    public car(String name, double speed, double weight) {
+        this.name = name;
+        this.speed = speed;
+        this.weight = weight;
+    }
+
+
+    public int compareTo(car c){
+       return 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof car)) return false;
+        car car = (car) o;
+        return Double.compare(car.speed, speed) == 0 && Double.compare(car.weight, weight) == 0 && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, speed, weight);
+    }
+}
+
+public class Common_Methods {
+    public static void main(String args[])
+    {
+        car c1 = new car("c1",100,500);
+        car c2 = new car("c1",100,500);
+        car c3 = new car("c3",200,300);
+
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+        System.out.println(c1.equals(c3));
+        System.out.println(c1.equals(c3));
+    }
+}
---
+++ src/Compare.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class human implements Comparable<human>{
+    public String name;
+    public int age;
+    public double height;
+
+    public human(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+
+    public  int compareTo(human m){
+        if (age == m.age)
+            return 0;
+        else if (age > m.age)
+            return 1;
+        else
+            return  -1;
+    }
+}
+
+public class Compare {
+    public static void main(String args[])
+    {
+        ArrayList<human> mk = new ArrayList<>();
+        mk.add(new human("nhan",23,1.72));
+        mk.add(new human("phuc",30,1.72));
+        mk.add(new human("hien",17,1.72));
+
+        Collections.sort(mk);
+        for(human h : mk)
+        {
+            System.out.println(h.age);
+        }
+    }
+}
